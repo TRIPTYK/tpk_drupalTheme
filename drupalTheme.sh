8e2f063b9a89d40d26b3fbc30b23d337ad3aee9c
maxime gosset <maxgosset@MacBook-Pro-de-maxime-2.local>
2016-01-27 09:07:51 +0100

    add gitignore

--- drupalTheme.sh
+++ drupalTheme.sh
@@ -12,7 +12,7 @@ if [ ! -d "default" ]; then
 
 	cd default/
 
-	drush si -y -d triptyk --db-url=mysql://admin:triptyk@$(docker-machine ip dockerdrupal):3306/triptykDb --db-su=admin --db-su-pw=triptyk --account-name=admin --account-pass=Y0sh1W11 --site-name="standard" ;
+	drush si -d triptyk --db-url=mysql://admin:triptyk@$(docker-machine ip dockerdrupal):3306/triptykDb --db-su=admin --db-su-pw=triptyk --account-name=admin --account-pass=Y0sh1W11 --site-name="standard" ;
 
 	drush en entity -y
 	drush en entityreference -y
@@ -39,10 +39,10 @@ if [ ! -d "default" ]; then
 		git init
 		git add . --a
 		git commit -m "first commit"
-		git remote add origin https://github.com/$user/$url.git
+		git remote add origin https://github.com/maxgosset/$url.git
 		git push -u origin master
 	fi
 
 else 
-		echo "le dossier default existe";
+		echo "le dossier default existe , veuillez vérifier son contenu";
 fi
